049769e909e4793f4cf0d06a3183a95eb4c4398b
mjcho08160 <mjcho08160@gmail.com>
2025-04-09 13:04:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const MY_DOMAIN = 'ksacsm.org';  // 원하는 도메인
-const MY_NOTION_PAGE = 'Korean-Society-at-American-College-of-Sports-Medicine-1d085ec0d66a80c999afd8e58153ba00';  // Notion 페이지 끝부분
+const MY_NOTION_PAGE = 'www.notion.so/Korean-Society-at-American-College-of-Sports-Medicine-1d085ec0d66a80c999afd8e58153ba00';  // Notion 페이지 끝부분
 
 const myNotionLink = `https://quilt-vulture-ac2.notion.site/${MY_NOTION_PAGE}?pvs=4`;
 
