87d48521e9c52993d4b094a33dd2c9400f670e2c
mjcho08160 <mjcho08160@gmail.com>
2025-04-09 10:46:37 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const MY_DOMAIN = 'ksacsm.org';  // 원하는 도메인
+const MY_NOTION_PAGE = '1d085ec0d66a80c999afd8e58153ba00';  // Notion 페이지 끝부분
+
+const myNotionLink = `https://quilt-vulture-ac2.notion.site/${MY_NOTION_PAGE}?pvs=4`;
+
+window.location.replace(`https://${MY_DOMAIN}/?url=${myNotionLink}`);
