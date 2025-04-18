bb00658f378212ed844f4494e57eedd7bae4693a
mjcho08160 <mjcho08160@gmail.com>
2025-04-17 23:06:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-const MY_DOMAIN = 'ksacsm.org';  // 원하는 도메인
-const MY_NOTION_PAGE = 'www.notion.so/Korean-Society-at-American-College-of-Sports-Medicine-1d085ec0d66a80c999afd8e58153ba00';  // Notion 페이지 끝부분
-
-const myNotionLink = `https://quilt-vulture-ac2.notion.site/${MY_NOTION_PAGE}?pvs=4`;
-
-window.location.replace(`https://${MY_DOMAIN}/?url=${myNotionLink}`);
+const MY_DOMAIN = 'ksacsm.vercel.app';  // 원하는 도메인
+const MY_NOTION_PAGE = '1d085ec0d66a80c999afd8e58153ba00';  // Notion 페이지 끝부분
+const PAGE_TITLE = "2025 KS-ACSM"
