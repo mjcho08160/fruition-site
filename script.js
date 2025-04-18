d6d80762d68201f7c5daca29b0758653878b0e16
mjcho08160 <mjcho08160@gmail.com>
2025-04-17 23:16:04 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-const MY_DOMAIN = 'ksacsm.vercel.app';  // 원하는 도메인
-const MY_NOTION_PAGE = '1d085ec0d66a80c999afd8e58153ba00';  // Notion 페이지 끝부분
-const PAGE_TITLE = "2025 KS-ACSM"
+const MY_DOMAIN = 'ksacsm.vercel.app';
+const MY_NOTION_PAGE = '1d085ec0d66a80c999afd8e58153ba00';
+const PAGE_TITLE = "2025 KS-ACSM";
