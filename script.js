1a333b44b8f1d6466e7614becfc257871e4db4b2
mjcho08160 <mjcho08160@gmail.com>
2025-04-21 14:42:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const MY_DOMAIN = 'ksacsm.vercel.app';
 const MY_NOTION_PAGE = '1d085ec0d66a80c999afd8e58153ba00';
-const PAGE_TITLE = "2025 KS-ACSM";
+const PAGE_TITLE = "2025 KS-ACSM Research Award";
